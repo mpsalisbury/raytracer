6c707e16792fdcc0a8645c68eb409b0f2f3916a5
Mike Salisbury <mike@salisburyclan.com>
2019-04-12 21:42:53 -0700

    Fiddle with RoundHoleCube.

--- src/main/java/projects/chapter16/RoundHoleCube.java
+++ src/main/java/projects/chapter16/RoundHoleCube.java
@@ -1,8 +1,10 @@
 package projects.chapter16;
 
 import raytracer.CSG;
+import raytracer.Color;
 import raytracer.Cube;
 import raytracer.Cylinder;
+import raytracer.Material;
 import raytracer.Matrix;
 import raytracer.Shape;
 import raytracer.Sphere;
@@ -18,23 +20,34 @@ public class RoundHoleCube {
   private static Shape sphere() {
     Shape sphere = Sphere.create();
     sphere.setTransform(Matrix.scaling(Math.sqrt(2)));
+    sphere.setMaterial(
+        Material.builder().setColor(Color.BLACK).setReflectivity(0.7).setSpecular(300).build());
     return sphere;
   }
 
   private static Shape cube() {
     Shape cube = Cube.create();
+    cube.setMaterial(
+        Material.builder()
+            .setColor(Color.create(0.7, 0.7, 0.7))
+            .setDiffuse(0.4)
+            .setReflectivity(0.2)
+            .build());
     return cube;
   }
 
   private static Shape holes() {
     Shape xHole = Cylinder.create();
     xHole.setTransform(Matrix.scaling(0.5, 1.1, 0.5).rotateZ(Math.PI / 2));
+    xHole.setMaterial(Material.builder().setColor(Color.create(0.9, 0.1, 0.1)).build());
 
     Shape yHole = Cylinder.create();
     yHole.setTransform(Matrix.scaling(0.5, 1.1, 0.5));
+    yHole.setMaterial(Material.builder().setColor(Color.create(0.1, 0.9, 0.1)).build());
 
     Shape zHole = Cylinder.create();
     zHole.setTransform(Matrix.scaling(0.5, 1.1, 0.5).rotateX(Math.PI / 2));
+    zHole.setMaterial(Material.builder().setColor(Color.create(0.1, 0.1, 0.9)).build());
 
     return CSG.createUnion(xHole, CSG.createUnion(yHole, zHole));
   }
--- src/main/java/projects/chapter16/RoundHoleCubeScene.java
+++ src/main/java/projects/chapter16/RoundHoleCubeScene.java
@@ -34,7 +34,7 @@ public class RoundHoleCubeScene {
         600,
         300,
         Math.PI / 3,
-        Tuple.createPoint(0, 1.5, -5),
+        Tuple.createPoint(0, 3.5, -5),
         Tuple.createPoint(0, 1, 0),
         Tuple.createVector(0, 1, 0));
   }
@@ -52,19 +52,18 @@ public class RoundHoleCubeScene {
     p.setMaterial(
         Material.builder()
             .setPattern(
-                Pattern.create3dCheckerBuilder(Color.create(1, 0.9, 0.9), Color.create(0, 0.1, 0.1))
+                Pattern.create3dCheckerBuilder(
+                        Color.create(0.8, 0.8, 0.8), Color.create(0.6, 0.6, 0.6))
                     .setTransform(Matrix.translation(0, -0.1, 0))
                     .build())
             .setSpecular(0)
-            .setReflectivity(0.5)
             .build());
     return p;
   }
 
   private static Shape createRoundHoleCube() {
     Shape rhc = RoundHoleCube.create();
-    rhc.setTransform(Matrix.translation(0, 1, 2).rotateY(Math.PI / 3));
-    rhc.setMaterial(Material.builder().setColor(Color.create(0.8, 0.1, 0.1)).build());
+    rhc.setTransform(Matrix.translation(0, 1, 1).rotateY(Math.PI / 5));
     return rhc;
   }
 
